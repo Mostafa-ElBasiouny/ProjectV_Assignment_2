0168e2586968034731a4bf432dd9d75f7cbbe3bd
Mostafa Elbasiouny <melbasiouny4793@conestogac.on.ca>
2023-09-19 18:55:31 -0400

    Implement the main function to read StudentData.txt ino a vector space.

--- ProjectV_Assignment_2/source.cpp
+++ ProjectV_Assignment_2/source.cpp
@@ -1,5 +1,44 @@
+#include <string>
+#include <vector>
+#include <fstream>
+#include <sstream>
 #include <iostream>
 
+using namespace std;
+
+struct StudentData
+{
+	string first;
+	string last;
+};
+
+vector<StudentData> student_data;
+
+void read_data(string file_name) {
+	fstream file;
+
+	file.open(file_name);
+
+	if (file.is_open()) {
+		while (!file.eof()) {
+			string line;
+			getline(file, line);
+
+			if (!line.empty()) {
+				stringstream line_stream(line);
+				StudentData student;
+
+				getline(line_stream, student.last, ',');
+				getline(line_stream, student.first, ',');
+
+				student_data.push_back(student);
+			}
+		}
+	}
+}
+
 int main() {
-	return 1;
+	read_data("C:\\Users\\Mostafa\\Desktop\\StudentData.txt");
+
+	return 0;
 }
