bb3a7877b13994573b5dccebf88e309968857bab
Mostafa Elbasiouny <melbasiouny4793@conestogac.on.ca>
2023-09-19 19:42:36 -0400

    Add support for PRE_RELEASE builds

--- ProjectV_Assignment_2/source.cpp
+++ ProjectV_Assignment_2/source.cpp
@@ -4,12 +4,18 @@
 #include <sstream>
 #include <iostream>
 
+#define _PRE_RELEASE
+
 using namespace std;
 
 struct StudentData
 {
 	string first;
 	string last;
+
+#ifdef _PRE_RELEASE
+	string email;
+#endif // _PRE_RELEASE
 };
 
 vector<StudentData> student_data;
@@ -31,18 +37,30 @@ void read_data(string file_name) {
 				getline(line_stream, student.last, ',');
 				getline(line_stream, student.first, ',');
 
+#ifdef _PRE_RELEASE
+				getline(line_stream, student.email, ',');
+#endif // _PRE_RELEASE
+
 				student_data.push_back(student); 
 
-#ifdef _DEBUG
+#if defined(_DEBUG) && defined(_PRE_RELEASE)
+				std::cout << student.last << ',' << student.first << ',' << student.email << std::endl;
+#elif _DEBUG
 				std::cout << student.last << ',' << student.first << std::endl;
-#endif // DEBUG
+#endif // DEBUG && PRE_RELEASE
 			}
 		}
 	}
 }
 
 int main() {
+#ifdef _PRE_RELEASE
+	std::cout << "Application is running pre-release source code." << std::endl;
+	read_data("C:\\Users\\Mostafa\\Desktop\\StudentData_Emails1.txt");
+#else
+	std::cout << "Application is running standard source code." << std::endl;
 	read_data("C:\\Users\\Mostafa\\Desktop\\StudentData.txt");
+#endif // _PRE_RELEASE
 
 	return 0;
 }
